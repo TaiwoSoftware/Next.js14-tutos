b5ec9240fe37de490478f087e6b62e2e9702f08f
Taiwo Akerele <davcodes24@gmail.com>
2024-04-14 08:17:06 -0700

    Learning next-themes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import MenuItem from "./MenuItem";
 import Link from 'next/link'
 import {AiFillHome} from 'react-icons/ai'
 import {BsFillInfoCircleFill} from 'react-icons/bs'
+import Switch from "./Switch";
 export default function Header() {
   return (
     <div className="flex justify-between items-center p-3 max-w-6xl mx-auto">
@@ -9,10 +10,13 @@ export default function Header() {
             <MenuItem  title="Home" address='/' Icon={AiFillHome} />
             <MenuItem  title="about" address='/about' Icon={BsFillInfoCircleFill} />
         </div>
+        <div className="flex items-center gap-4">
+          <Switch />
         <Link href={'/'} className="flex gap-1 items-center">
           <span className="text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg">IMDb</span>
           <span className="text-xl hidden sm:inline">Clone</span>
         </Link>
+        </div>
     </div>
   )
 }
