2b464b6054f62e4f6b2beb58f453cb5d59343b28
Taiwo Akerele <davcodes24@gmail.com>
2024-04-22 17:38:17 -0700

    Successfully fetched the data from the TMBD website it is time to style the interface

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,20 @@
-export default function page() {
-  return (
-    <div></div>
-  )
+import Results from "@/components/Results";
+
+const API_KEY = process.env.API_KEY;
+export default async function page({ searchParams }) {
+  const genre = searchParams.genre || "fetchTrending";
+  const res = await fetch(
+    `https://api.themoviedb.org/3${
+      genre === "fetchTopRated" ? `/movie/top-rated` : `/trending/all/week`
+    }?api_key=${API_KEY} language=en-US&page=1`
+  );
+  const data = res.json();
+  if (!res.ok) {
+    throw new Error("Failed to fetch");
+  }
+  const results = data.results;
+  console.log(results.title);
+  return <div>
+    <Results results={results} />
+  </div>;
 }
